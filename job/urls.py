b01a02bf4648c8a359e902e535775258261330ad
azzamrafa <azzamrafa2016@gmail.com>
2020-08-08 07:21:59 -0700

    add slug & image field

--- job/urls.py
+++ job/urls.py
@@ -1,8 +1,11 @@
 from . import views
 from django.urls import path, include
 
+
+app_name = 'job'
+
 urlpatterns = [
    
     path('job_list', views.job_list),
-    path('<int:id>', views.job_detail),
+    path('<str:slug>', views.job_detail, name='job_detail'),
 ]
